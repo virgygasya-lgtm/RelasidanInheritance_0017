217f82bc20c6766d1a5487193c88186c325bbaa2
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 10:37:03 +0700

    .Inisialisasi Struktur Class.cpp

---
+++ .Inisialisasi Struktur Class.cpp
@@ -0,0 +1,12 @@
+#ifndef IBU_H
+#define IBU_H
+#include <vector>
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
--- awal agregasi ibu.cpp
+++
@@ -1,3 +0,0 @@
-#ifndef IBU_H
-#define IBU_H
-#include <vector>
