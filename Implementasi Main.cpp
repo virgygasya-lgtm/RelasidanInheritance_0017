9884c50d673ce4222f5c268735e5529ced8d4ca7
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 11:11:37 +0700

    Implementasi Main.cpp

--- Implementasi Main.cpp
+++ Implementasi Main.cpp
@@ -20,3 +20,10 @@ public:
 
 };
 
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
