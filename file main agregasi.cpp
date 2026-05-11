e2459157f4e2f8aa041cfd8cd88b9a6cbfb813d9
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 10:59:48 +0700

    file main agregasi.cpp

--- file main agregasi.cpp
+++ file main agregasi.cpp
@@ -11,6 +11,14 @@ int main() { //agregasi
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
     delete varIbu;
     delete varibu2;
     delete varAnak1;
