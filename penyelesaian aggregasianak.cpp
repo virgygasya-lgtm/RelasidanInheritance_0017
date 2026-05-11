7ac0da80fdbc93c0c345167d582d9aa9b550badf
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 10:30:11 +0700

    penyelesaian aggregasianak.cpp

--- penyelesaian aggregasianak.cpp
+++ penyelesaian aggregasianak.cpp
@@ -7,7 +7,6 @@ public:
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
-
     ~anak() {
         cout << "Anak \"" << nama << "\" tidak ada\n";
     }
