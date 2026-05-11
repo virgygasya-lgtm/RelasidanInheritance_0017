19d017876a3a11891e209e68a92e74eb152a2172
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 11:12:52 +0700

    Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -20,6 +20,21 @@ public:
 
 };
 
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
+
 int main() {
     pelajar siswa1("andi laksono", "SMAN 1 Bantul");
     cout << siswa1.perkenalan();
