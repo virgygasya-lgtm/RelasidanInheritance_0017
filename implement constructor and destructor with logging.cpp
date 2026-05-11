17f308fcf4a509702663b900a0ad3a4df9f8fc31
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 10:28:21 +0700

    implement constructor and destructor with logging.cpp

--- implement constructor and destructor with logging.cpp
+++ implement constructor and destructor with logging.cpp
@@ -7,3 +7,9 @@ public:
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
