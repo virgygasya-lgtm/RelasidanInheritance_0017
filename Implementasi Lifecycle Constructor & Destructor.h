9b71a46cc1906ec299488d1576bfd3a2ec5cd3a8
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 11:16:54 +0700

    Implementasi Lifecycle Constructor & Destructor.h

---
+++ Implementasi Lifecycle Constructor & Destructor.h
@@ -0,0 +1,18 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+
+};
+#endif
+
+// Masukkan ke dalam public class jantung
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
