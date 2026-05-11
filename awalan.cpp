bcfb42aa622057f2800571e3c611c9f443f33597
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-11 10:26:34 +0700

    define anak class structure and header guards

--- awalan.cpp
+++
@@ -1,2 +0,0 @@
-#ifndef ANAK_H
-#define ANAK_H
---
+++ define anak class structure and header guards.cpp
@@ -0,0 +1,9 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
